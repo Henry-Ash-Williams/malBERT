b5329ba1f0f56e07f47074b9678ef1c5c5507867
Henry Williams <henrywilliams95000@gmail.com>
2025-07-09 08:54:50 +0100

    refined obfuscation experiment

--- src/obfuscation-experiment.py
+++ src/obfuscation-experiment.py
@@ -1,33 +1,102 @@
+import json
 import torch
 import wandb 
 import r2pipe 
 import numpy as np 
-from tqdm import trange, tqdm
+from tqdm import tqdm
 import torch.nn.functional as F
 from sklearn.metrics import classification_report
 from transformers import RobertaForSequenceClassification, PreTrainedTokenizerFast
 
 import os
-import json
 import random
 import tempfile
 import subprocess
 from os import PathLike
+from argparse import ArgumentParser
+from typing import Dict, List, Tuple
 
 device = torch.device('mps')
 
-MAL_PATH = "/Volumes/malware-dataset/unobfuscated-exes/"
-BEN_PATH = "/Volumes/malware-dataset/benign_program_dataset_WinXP_SP3/benign_programs_WinXP_SP3"
+RUNS = {
+    'frozen_pretrained': "t4o7wvla",
+    'pretrained': "e1tosi4k",
+    'base': "e96b8h5a"
+}
 
-def get_model(id):
+os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+os.environ['WANDB_SILENT'] = "true"
+
+def get_args():
+    parser = ArgumentParser(
+        prog="Obfuscation Experiment",
+        description="TODO: Write a description",
+    )
+    
+    parser.add_argument(
+        "target", 
+        choices=['malicious', 'benign', 'all'],
+        default='all',
+        type=str,
+        help="The class of executables to be obfuscated"
+    )
+    
+    parser.add_argument(
+        '-m', '--model', 
+        choices=['frozen_pretrained', 'pretrained', 'base'], 
+        default='base', 
+        type=str, 
+        help='The model used by the experiment'
+    )
+    
+    parser.add_argument(
+        '-b', '--batch-size',
+        type=int, 
+        default=512, 
+        help="Batch size"
+    )
+    
+    parser.add_argument(
+        '-o', '--output', 
+        type=str,
+        help='Results output location', 
+    )
+
+    parser.add_argument(
+        '-s', '--steps', 
+        type=int, 
+        default=10, 
+        help='Number of experiment steps',
+    )
+    
+    parser.add_argument(
+        '-f', '--files', 
+        type=str, 
+        help="Path to a JSON file containing the locations and labels of files used by the experiment", 
+        default="/Users/henrywilliams/Documents/programming/python/ai/malbert-test/notebooks/obfuscation-experiment-files.json",
+    )
+    
+    parser.add_argument(
+        '-t', '--tokenizer-path', 
+        type=str, 
+        help='Path to the tokenizer', 
+        default="/Users/henrywilliams/Documents/programming/python/ai/malbert-test/MalBERTa",
+    )
+
+    args = parser.parse_args()    
+    return args
+    
+    
+def get_model(id: str) -> RobertaForSequenceClassification:
+    api = wandb.Api() 
+
     with tempfile.TemporaryDirectory() as tdir: 
         artifact = api.artifact(f'henry-williams/opcode-malberta/model-{id}:v1', type='model')
-        base_path = artifact.download(root=tdir)
+        path = artifact.download(root=tdir)
 
-        return RobertaForSequenceClassification.from_pretrained(base_path)
+        return RobertaForSequenceClassification.from_pretrained(path)
 
-
-def get_disassm(path: PathLike):
+def get_disassm(path: PathLike) -> List[Dict[str, str]]:
     r2 = r2pipe.open(path, ['-12'])
     r2.cmd("aaa")
 
@@ -54,8 +123,7 @@ def get_disassm(path: PathLike):
 
     return full_disassembly
 
-
-def make_opcode_sequences(instrs):
+def make_opcode_sequences(instrs: List[Dict[str, str]]) -> List[str]:
     valid_instrs = [
         instruction["inst"]
         for instruction in instrs
@@ -64,7 +132,7 @@ def make_opcode_sequences(instrs):
 
     return ' '.join([instr.split(' ')[0] for instr in valid_instrs])
 
-def tokenize(sample, model):
+def tokenize(sample: str, model: RobertaForSequenceClassification) -> Dict[str, List[int]]:
     seq_length = model.config.max_position_embeddings
 
     input = tokenizer(
@@ -79,9 +147,10 @@ def tokenize(sample, model):
 
     return input
 
-def pipeline(model, path: os.PathLike, batch_size=64):
+def pipeline(args, model: RobertaForSequenceClassification, path: os.PathLike) -> Tuple[torch.Tensor, torch.Tensor]:
     if not os.path.exists(path):
         raise Exception(f"Could not find specified file at {path}")
+
     disassembly = get_disassm(path)
     opcodes = make_opcode_sequences(disassembly)
     input = tokenize(opcodes, model)
@@ -89,10 +158,12 @@ def pipeline(model, path: os.PathLike, batch_size=64):
     model.eval()
 
     logits = []
-    input_ids = input['input_ids'].split(batch_size)
-    attention_mask = input['attention_mask'].split(batch_size)
-    token_type_ids = input['token_type_ids'].split(batch_size)
+    input_ids = input['input_ids'].split(args.batch_size)
+    attention_mask = input['attention_mask'].split(args.batch_size)
+    token_type_ids = input['token_type_ids'].split(args.batch_size)
+
     torch.mps.empty_cache()
+
     for ids, attn_mask, tok_ty_ids in tqdm(zip(input_ids, attention_mask, token_type_ids), total=len(input_ids), position=2, leave=False, desc=path.split('/')[-1]):
         ids = ids.to(device)
         attn_mask = attn_mask.to(device)
@@ -106,63 +177,75 @@ def pipeline(model, path: os.PathLike, batch_size=64):
             ))
     
     logits = torch.vstack([logit.logits for logit in logits])
-    return F.softmax(logits.mean(dim=0), dim=0)
+    # return F.softmax(logits.mean(dim=0), dim=0)
+    return logits
 
-
-def experiment_step(model, file, p=1.0, batch_size=64):
+def obfuscate(file: os.PathLike, output: os.PathLike) -> os.PathLike:
+    obfuscated_path = os.path.join(output, file.split('/')[-1].replace('.exe', '.obfs.exe'))
+    subprocess.run(['upx', '-o', obfuscated_path, file], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    return obfuscated_path
+    
+def experiment_step(args, model: RobertaForSequenceClassification, file: os.PathLike, label: int, p: float = 1.0) -> Tuple[torch.Tensor, torch.Tensor]:
     ''' 
     p is the likelihood of the file being obfuscated
     '''
+    obfuscated = False
     with tempfile.TemporaryDirectory() as tdir: 
-        if random.random() < p: 
-            obfuscated_path = os.path.join(tdir, file.split('/')[-1])
-            subprocess.run(['upx', '-o', obfuscated_path, file], stdout=subprocess.DEVNULL, stderror=subprocess.DEVNULL)
-            return pipeline(model, obfuscated_path, batch_size=batch_size)
-        else: 
-            return pipeline(model, file, batch_size=batch_size)
+        q = random.random()
 
+        if args.target == 'all' or \
+            (args.target == "malicious" and label == 1) or \
+            (args.target == "benign" and label == 0): 
 
-def run_experiment(files, labels, model, p=0.0, batch_size=256):
+            if q < p: 
+                file = obfuscate(file, tdir)
+                obfuscated = True
+
+            return pipeline(args, model, file), obfuscated
+
+def run_experiment(args, files: List[os.PathLike], labels: List[int], model: RobertaForSequenceClassification, step: int, p: float = 0.0) -> Dict[str, Dict[str, float] | float]:
     predicted = []
     actual = []
 
-    for file, label in tqdm(zip(files, labels), total=len(files), leave=False, desc=f'p = {p:.2}', position=1): 
-        try: 
-            logits = experiment_step(model, file, p=p, batch_size=batch_size)
-        except: 
-            continue 
+    os.makedirs(os.path.join(args.output, f"step-{i}"), exist_ok=True)
 
-        predicted.append(logits.argmax().item())
+    for file, label in tqdm(zip(files, labels), total=len(files), leave=False, desc=f'p = {p:.2}', position=1): 
+        # try: 
+        #     logits, was_obfuscated = experiment_step(args, model, file, label, p=p)
+        # except: 
+        #     continue 
+        logits, was_obfuscated = experiment_step(args, model, file, label, p=p)
+
+        output_logits_path = os.path.join(args.output, f"step-{i}", f"{file.split('/')[-1]}-logits{'-obfs' if was_obfuscated else ''}.pt")
+        torch.save(logits, output_logits_path)
+        class_likelihood = F.softmax(logits.mean(dim=0), dim=0)
+
+        predicted.append(class_likelihood.argmax().item())
         actual.append(label)
 
     return classification_report(actual, predicted, output_dict=True)
 
 if __name__ == "__main__":
-    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
-    os.environ['WANDB_SILENT'] = "true"
+    args = get_args()
+    model = get_model(RUNS[args.model]).to(device)
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(args.tokenizer_path)
 
-    api = wandb.Api() 
-    runs = {
-        'frozen_pretrained': "t4o7wvla",
-        'pretrained': "e1tosi4k",
-        'base': "e96b8h5a"
-    }
-    model = get_model(runs['base']).to(device)
-    tokenizer = PreTrainedTokenizerFast.from_pretrained("/Users/henrywilliams/Documents/programming/python/ai/malbert-test/MalBERTa")
-    obfuscation_likelihood = np.linspace(0, 1, 20)
+    os.makedirs(args.output, exist_ok=True)
 
-    ben_files = [os.path.join(BEN_PATH, file) for file in os.listdir(BEN_PATH) if file.endswith('.exe') and not file.startswith('._')]
-    mal_files = [os.path.join(MAL_PATH, file) for file in os.listdir(MAL_PATH) if file.endswith('.exe') and not file.startswith('._')]
-    random.shuffle(ben_files)
-    ben_files = ben_files[:len(mal_files)]
+    with open(os.path.join(args.output, 'config.json'), 'w') as f: 
+        json.dump(args.__dict__, f)
+    
+    with open(args.files, 'r') as file: 
+        data = json.load(file)
 
-    files = mal_files + ben_files 
-    labels = [1] * len(mal_files) + [0] * len(ben_files)
-
+    files = list(data.keys())
+    labels = list(data.values())
+    
+    obfuscation_likelihood = np.linspace(0.0, 1.0, args.steps)
     results = []
 
-    for p in tqdm(obfuscation_likelihood, position=0, desc='Running...'):
-        results.append(run_experiment(files, labels, model, p, batch_size=512))
+    for i, p in tqdm(enumerate(obfuscation_likelihood), position=0, desc='Running...', total=len(obfuscation_likelihood)):
+        results.append(run_experiment(args, files, labels, model, i, p))
 
-    with open('obfuscation-results.json', 'w') as file: 
-        json.dump(results, file)
+    with open(os.path.join(args.output, 'results.json'), 'w') as f: 
+        json.dump(results, f)
